5a0e001a6efe1e462ce8de086786b0ff7181856a
b72u68 <mdinh@hawk.iit.edu>
2020-06-10 00:14:57 -0500

    Add C++ solution for max subarray sum

---
+++ maxSubarraySum1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxSubarraySum(vector<int> nums) {
+    int best = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i; j < nums.size(); j++) {
+            int sum = 0;
+            for (int k = i; k < j; k++) {
+                sum += nums.at(k); 
+            }
+            best = max(best, sum);
+        }
+    }
+    return best;
+}
