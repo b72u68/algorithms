1a04990739e5fb40743e6b609877537ceb8ab054
b72u68 <mdinh@hawk.iit.edu>
2020-06-10 22:37:08 -0500

    Fix error

--- maxSubarraySum3.cpp
+++ maxSubarraySum3.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int maxSubarraySum(vector<int> nums) {
-    int best = 0;
     int sum = 0;
+    int best = 0;
     for (int i = 0; i < nums.size(); i++) {
         sum = max(nums.at(i), sum + nums.at(i));
         best = max(best, sum);
