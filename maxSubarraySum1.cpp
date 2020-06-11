bc4683cb925687a17693c311e9ad40549e89325b
b72u68 <mdinh@hawk.iit.edu>
2020-06-10 21:54:22 -0500

    Fix small error

--- maxSubarraySum1.cpp
+++ maxSubarraySum1.cpp
@@ -7,7 +7,7 @@ int maxSubarraySum(vector<int> nums) {
     for (int i = 0; i < nums.size(); i++) {
         for (int j = i; j < nums.size(); j++) {
             int sum = 0;
-            for (int k = i; k < j; k++) {
+            for (int k = i; k <= j; k++) {
                 sum += nums.at(k); 
             }
             best = max(best, sum);
