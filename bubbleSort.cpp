d02bb8efe1b49315224219fd0125cd8541d6504a
b72u68 <mdinh@hawk.iit.edu>
2020-06-11 20:02:06 -0500

    Add sorting algorithm

---
+++ bubbleSort.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> bubbleSort(vector<int> nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = 0; j < nums.size()-1; j++) {
+            if (nums.at(j) > nums.at(j+1)) {
+                int temp = nums.at(j);
+                nums.at(j) = nums.at(j+1);
+                nums.at(j+1) = temp;
+            }
+        }
+    }
+    return nums;
+}
