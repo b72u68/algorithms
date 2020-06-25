7d3c0b9dc5218ec8395ac84fa7eba1f020029664
b72u68 <mdinh@hawk.iit.edu>
2020-06-25 13:23:59 -0500

    Clean file directory

--- maxSubarraySum1.py
+++
@@ -1,9 +0,0 @@
-def maxSubArraySum(lst):
-    best = 0
-    for i in range(len(lst)):
-        for j in range(i, len(lst)):
-            sum = 0 
-            for j in range(i, j):
-                sum += lst[k]
-            best = max(best, sum)
-    return best
--- maxSubarraySum2.py
+++
@@ -1,8 +0,0 @@
-def maxSubarraySum(lst):
-    best = 0
-    for i in range(len(lst)):
-        sum = 0
-        for j in range(i, len(lst)):
-            sum += lst[j]
-            best = max(sum, best)
-    return best
