6a935d57011c1a262494cba6c6ddc2d0f57f4a4c
b72u68 <mdinh@hawk.iit.edu>
2020-10-08 20:41:38 -0500

    Cleaned repo

--- Shopee Code/JudgingServers.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-
-for i in range(T):
-    S, N = input().split()
-    P = input().split()
-    P = sorted([int(x) for x in P])
-    cost = sum(P[:int(N)-1])
-    print(f'Case {i+1}: {cost}')
